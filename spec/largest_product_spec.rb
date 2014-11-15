1f12fcbf4e5de69edef3cf7f2049c944b10694ff
Julie Torres <reeddetorres@gmail.com>
2014-11-15 08:43:38 -0500

    Comment out smallest multiple test

---
+++ spec/largest_product_spec.rb
@@ -0,0 +1,10 @@
+require 'largest_product'
+
+describe 'LargestProduct' do
+  context 'has a method called multiply_array' do
+    it 'returns 120 for the array [2, 3, 4, 5]' do
+      result = multiply_array(array)
+      expect(result).to eq 120
+    end
+  end
+end
