05b4bc5f9635fe4fdcc58efc9efb6255c1019a5e
Julie Torres <reeddetorres@gmail.com>
2014-11-16 18:56:57 -0500

    Modify tests and previous methods

--- spec/largest_product_spec.rb
+++ spec/largest_product_spec.rb
@@ -8,9 +8,9 @@ describe 'LargestProduct' do
     end
   end
 
-  context 'has a method called find_sub_array' do
+  context 'has a method called find_array1' do
     it 'finds the first 4 elements of an array' do
-      result = find_sub_array(0, 4, [1, 2, 3, 4, 5, 6])
+      result = find_array1(0, 4, [1, 2, 3, 4, 5, 6])
       expect(result).to match_array([1, 2, 3, 4])
     end
   end
@@ -22,11 +22,18 @@ describe 'LargestProduct' do
     end
   end
 
-  context 'when no sub-arrays have been eliminated' do
-    it 'the method next_array returns the next 4 elements' do
+  context 'has a method called next_array' do
+    it 'returns the next 4 elements' do
       result = next_array([1, 2, 3, 4, 5, 6], 0, 4)
       expect(result).to match_array([2, 3, 4, 5])
     end
   end
 
+  context 'has a method called largest_array' do
+    it 'returns an empty array when no arrays have been eliminated' do
+      result = largest_array
+      expect(result).to match_array([1, 2, 3, 4])
+      next_arr = next_array([1, 2, 3, 4, 5, 6], 0, 4)
+    end
+  end
 end
